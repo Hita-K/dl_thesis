d6ad118953e2e7f09151562dbc64fb9dbd150bc0
Hita-K <68398983+Hita-K@users.noreply.github.com>
2022-04-20 23:07:42 -0400

    New approach (model)

--- lstm.py
+++ lstm.py
@@ -9,7 +9,7 @@ import torch.optim as optim
 import pickle
 import sys
 from concurrent.futures import ProcessPoolExecutor, as_completed
-
+import torch.nn.functional as F
 import biosppy.signals.tools as st
 import numpy as np
 import os
@@ -55,33 +55,59 @@ y = []
 for j in range(len(labels)):
     if j < before or (j + 1 + after) > len(signals) / float(sample):
         continue
+    # print(len(signals))
     signal = signals[int((j - before) * sample):int((j + 1 + after) * sample)]
-    signal, _, _ = st.filter_signal(signal, ftype='FIR', band='bandpass', order=int(0.3 * fs),
-                                frequency=[3, 45], sampling_rate=fs)
-    rpeaks, = hamilton_segmenter(signal, sampling_rate=fs)
-    rpeaks, = correct_rpeaks(signal, rpeaks=rpeaks, sampling_rate=fs, tol=0.1)
-    if len(rpeaks) / (1 + after + before) < 40 or \
-            len(rpeaks) / (1 + after + before) > 200:  # Remove abnormal R peaks signal
-        continue
-    # Extract RRI, Ampl signal
-    rri_tm, rri_signal = rpeaks[1:] / float(fs), np.diff(rpeaks) / float(fs)
-    rri_signal = medfilt(rri_signal, kernel_size=3)
-    ampl_tm, ampl_siganl = rpeaks / float(fs), signal[rpeaks]
-    hr = 60 / rri_signal
-    # Remove physiologically impossible HR signal
-    if np.all(np.logical_and(hr >= hr_min, hr <= hr_max)):
-        # Save extracted signal
-        X.append([(rri_tm, rri_signal), (ampl_tm, ampl_siganl)])
-        y.append(0. if labels[j] == 'N' else 1.)
+    # print(len(signal))
+    # signal, _, _ = st.filter_signal(signal, ftype='FIR', band='bandpass', order=int(0.3 * fs),
+    #                             frequency=[3, 45], sampling_rate=fs)
+    # rpeaks, = hamilton_segmenter(signal, sampling_rate=fs)
+    # rpeaks, = correct_rpeaks(signal, rpeaks=rpeaks, sampling_rate=fs, tol=0.1)
+    # if len(rpeaks) / (1 + after + before) < 40 or \
+    #         len(rpeaks) / (1 + after + before) > 200:  # Remove abnormal R peaks signal
+    #     continue
+    # # Extract RRI, Ampl signal
+    # rri_tm, rri_signal = rpeaks[1:] / float(fs), np.diff(rpeaks) / float(fs)
+    # rri_signal = medfilt(rri_signal, kernel_size=3)
+    # ampl_tm, ampl_siganl = rpeaks / float(fs), signal[rpeaks]
+    # hr = 60 / rri_signal
+    # # Remove physiologically impossible HR signal
+    # if np.all(np.logical_and(hr >= hr_min, hr <= hr_max)):
+    #     # Save extracted signal
+    X.append([signal])
+    # X.append([(rri_tm, rri_signal), (ampl_tm, ampl_siganl)])
+    y.append(0. if labels[j] == 'N' else 1.)
 
 
-EMBED_DIM = 6
-HIDDEN_DIM = 6
+EMBED_DIM = 1
+HIDDEN_DIM = 2
 
-model = nn.LSTM(EMBED_DIM, HIDDEN_DIM)
+# in forward, call lstm
+# nn.linear (hidden_dim, 1)
+# sigmoid
+# out = out[-1] in forward + unsqueeze
+
+class LSTMECG(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim):
+        super(LSTMECG, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.embedding_dim = embedding_dim
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+        self.hidden = nn.Linear(hidden_dim, 1)
+    def forward(self, sig):
+        lstm_out, _ = self.lstm(sig.view(len(sig), 1, -1))
+        lstm_out = lstm_out[-1].unsqueeze(0)
+        new = self.hidden(lstm_out.view(len(signal), -1))
+        score = F.sigmoid(new)
+        return score
+
+model = LSTMECG(EMBED_DIM, HIDDEN_DIM)
 loss_function = nn.BCELoss()
 optimizer = optim.SGD(model.parameters(), lr=0.1)
-
+X = torch.tensor(X, dtype=torch.float32)
+y = torch.tensor(y, dtype=torch.float32)
+print("X", X)
+print("y", y)
+print(X.shape, y.shape)
 # wfbd in seperate function
 for epoch in range(100):
     print("In Epoch")
@@ -93,10 +119,14 @@ for epoch in range(100):
         print("finished zero grad")
 
         # forward pass 
-        ecg_scores = model(torch.tensor(sig))
+        sigT = sig.t()
+        
+        ecg_scores = model(sig.unsqueeze(0))
 
         print("ecg_scores")
         # loss, gradient, update params 
+        print("ecg_scores", ecg_scores)
+        print(lab)
         loss = loss_function(ecg_scores, lab)
         loss.backward() # only do when there is a label 
         print("loss")
